34ff232cf1d94b8a6073553ea4dc9ce494e0d6c2
MatthijsBreemans <matthijs@groov.nl>
2016-10-25 17:04:56 +0200

    Removed IDisposable, fixed properties on mailgun extensionmethod
    Added development branch

---
+++ src/Postbode.Smtp/SmtpDeliveryService.cs
@@ -0,0 +1,19 @@
+﻿using Postbode.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Postbode.Smtp
+{
+    public class SmtpDeliveryService : IDeliveryService
+    {
+        public string Name => "System.Net.Smtp";
+
+        public Task<IResponse> SendAsync(IPostbode service = null)
+        {
+            throw new NotSupportedException("Waiting for .NET Core 1.2.0");
+        }
+    }
+}
