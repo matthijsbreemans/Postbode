0239a9da160cdda5afa9e0d176e0a24fbca1a38c
MatthijsBreemans <matthijs@groov.nl>
2016-10-28 14:17:43 +0200

    Adedd test

---
+++ test/Postbode.Test/TestSendgrid.cs
@@ -0,0 +1,21 @@
+﻿using Postbode.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Postbode.Interfaces;
+using Postbode.Sendgrid;
+using Xunit;
+
+namespace Postbode.Test
+{
+    public class TestSendgrid
+    {
+        [Fact]
+        public async void SimpelTest()
+        {
+
+
+        }
+    }
+}
