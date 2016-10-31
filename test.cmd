4f466c412e14065605bcf60a001e80956fed2ad3
MatthijsBreemans <matthijs@groov.nl>
2016-10-31 16:21:37 +0100

    appveyor

--- test.cmd
+++ test.cmd
@@ -1,6 +1,5 @@
 nuget install OpenCover -Version 4.6.519 -OutputDirectory tools
 nuget install coveralls.io -Version 1.3.4 -OutputDirectory tools
 
-.\tools\OpenCover\4.6.519\tools\opencover.console.exe -register:user -oldStyle -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test test/Postbode.Test" -output:"coverage.xml"
-
+.\tools\OpenCover\4.6.519\tools\OpenCover.Console.exe -register:user -oldStyle -target:"C:/Program Files/dotnet/dotnet.exe" -targetargs:"test test/Postbode.Test" -output:"coverage.xml"
 .\tools\coveralls.io\1.3.4\tools\coveralls.net.exe --opencover coverage.xml
