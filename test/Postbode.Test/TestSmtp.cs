d803f64b9cbf75302c9c8224801607816db2b89f
MatthijsBreemans <matthijs@groov.nl>
2016-11-01 14:01:38 +0100

    Added unittests

---
+++ test/Postbode.Test/TestSmtp.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication;
+using System.Threading.Tasks;
+using Postbode.Content;
+using Postbode.Smtp;
+using Xunit;
+
+namespace Postbode.Test
+{
+
+    public class TestSmtp
+    {
+        [Fact]
+        public async void TestSmtpDelivery()
+        {
+            var postbode = new PostbodeClient().SetSender("test@test.nl").SetRecipient("test2@test2.nl", type: RecipientType.Cc)
+                .SetTextContent("test").Use(new SmtpDeliveryService());
+            await Assert.ThrowsAsync<NotImplementedException>(() => postbode.SendAsync());
+        }
+
+    }
+}
