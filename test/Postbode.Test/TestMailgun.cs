cbc6719a4ae1069a9560188c19615a736eb62775
MatthijsBreemans <matthijs@groov.nl>
2016-11-01 16:28:30 +0100

    More tests

--- test/Postbode.Test/TestMailgun.cs
+++ test/Postbode.Test/TestMailgun.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Postbode.Content;
 using Postbode.Mailgun;
@@ -14,11 +15,37 @@ namespace Postbode.Test
         [Fact]
         public async void MailgunTest()
         {
-            var mailgunClient = new MailgunDeliveryService(domain: "test.nl" ,apikey: "0x000", httpClient: new TestHttpClient());
-            var postbode = new PostbodeClient();
-            var result = await postbode.Use(mailgunClient).SetRecipient("to@example.org").SetSender("sender@example.org").SetSubject("test").SetTextContent("HALLLOOOO").SendAsync();
+            var mailgunClient = new MailgunDeliveryService(domain: "test.nl", apikey: "0x000", httpClient: new TestHttpClient(HttpStatusCode.OK, ""));
+            IPostbode postbode = new PostbodeClient();
+            postbode =
+                postbode.Use(mailgunClient)
+                    .SetRecipient("to@example.org")
+                    .SetSender("sender@example.org")
+                    .SetSubject("test")
+                    .SetTextContent("HALLLOOOO");
 
+            postbode.Mail.Headers = new Dictionary<string, string>() { { "test-key", "test-value" } };
+
+            var result = await postbode.SendAsync();
             Assert.True(result.Succes);
+        }
+
+        [Fact]
+        public async void MailgunFailTest()
+        {
+            var mailgunClient = new MailgunDeliveryService(domain: "test.nl", apikey: "0x000", httpClient: new TestHttpClient(HttpStatusCode.BadRequest, ""));
+            IPostbode postbode = new PostbodeClient();
+            postbode =
+                postbode.Use(mailgunClient)
+                    .SetRecipient("to@example.org")
+                    .SetSender("sender@example.org")
+                    .SetSubject("test")
+                    .SetTextContent("HALLLOOOO");
+
+            postbode.Mail.Headers = new Dictionary<string, string>() { { "test-key", "test-value" } };
+
+            var result = await postbode.SendAsync();
+            Assert.False(result.Succes);
         }
     }
 }
